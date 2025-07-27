b46dcabdc79457460f9e1be983d877fc3ccaa5a9
Stephan Zehrer <stephan@zehrer.xyz>
2025-07-27 15:00:23 +0200

    Inital version generated by XCode 26 Beta 4

---
+++ SwiftFlash/SwiftFlash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftFlash
+//
+//  Created by Stephan Zehrer on 27.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftFlash/SwiftFlash/SwiftFlashApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFlashApp.swift
+//  SwiftFlash
+//
+//  Created by Stephan Zehrer on 27.07.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFlashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
